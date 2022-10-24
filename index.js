9ef9c7b40243f1fbb9b1561788a5427f8c430c7b
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-10-24 11:09:50 +0900

    [Update]config: Read Wifi_Config.json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var Wifi = require('rpi-wifi-connection');
-var wifi = new Wifi();
- 
-wifi.connect({ssid:'my-network', psk:'raspberry'}).then(() => {
+const Wifi = require('rpi-wifi-connection');
+const wifi = new Wifi();
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('/home/pi/raspberrypi/Wi-fi_Config.json', 'utf8'));
+
+wifi.connect({ssid:data.ssid, psk:data.password}).then(() => {
     console.log('Connected to network.');
 })
 .catch((error) => {
